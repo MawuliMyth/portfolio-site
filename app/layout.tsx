c52fcb07dab1a817abe0be0d8d977162f14acc1b
manueldiamond <manueldiamondlistowell@gmail.com>
2025-06-09 20:12:03 +0000

    cc

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
       </Head>
       <body className={`${inter.className} pt-28 sm:pt-36 bg-black text-white height-[5000px]`}>
         <div className="pink-blur -z-10 bg-[red] absolute top-[-6rem] right-[11.25rem] w-[31.25rem] h-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-        <div className="blue-blur -z-10 bg-slate-500 absolute top-[-1rem]  left-[-35.25rem] w-[50.25rem] h-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:[-5rem]"></div>
+        <div className="blue-blur -z-10 bg-red-200/30 absolute top-[-1rem]  left-[-35.25rem] w-[50.25rem] h-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:[-5rem]"></div>
         <ActiveSectionContextProvider>
           <Header />
           {children}
